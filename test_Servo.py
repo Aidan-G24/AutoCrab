4c5f7f88b9fc98d8948fac08b3be3501f23ddb3c
Team7 <agowe@g.clemson.edu>
2023-10-02 12:35:11 -0400

    test

---
+++ test_Servo.py
@@ -0,0 +1,16 @@
+
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(25)
+
+try:
+	while True:
+		servo.min()
+		sleep(1)
+		servo.mid()
+		sleep(1)
+		servo.max()
+		sleep(1)
+except KeyboardInterrupt:
+	print("Stop")

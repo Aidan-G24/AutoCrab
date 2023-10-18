3217d202fef4a5fd7a00121d5587ed3428911480
Aidan-G24 <aidangowe@gmail.com>
2023-10-18 13:48:26 -0400

    bug fix

--- test_Servo.py
+++ test_Servo.py
@@ -11,11 +11,14 @@ servo = Servo(('front', 'left'), 25, pi)
 
 try:
 	while True:
+		print('angle 0')
 		servo.set_angle(0)
-		sleep(2)
-		servo.mid(90)
-		sleep(2)
-		servo.max(180)
-		sleep(2)
+		sleep(5)
+		print('angle 90')
+		servo.set_angle(90)
+		sleep(5)
+		print('angle 180')
+		servo.set_angle(180)
+		sleep(5)
 except KeyboardInterrupt:
 	servo.servo_off()

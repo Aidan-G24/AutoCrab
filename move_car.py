81e02fd81bd3217bb4d355efe7f790b98f1580e8
Aidan-G24 <aidangowe@gmail.com>
2023-11-01 14:18:45 -0400

    car move

--- move_car.py
+++ move_car.py
@@ -12,8 +12,18 @@ if __name__ == "__main__":
 
 	try:
 		print("Start Move forward")
-		Crab.car_move("forward", 75, 1000)
-		time.sleep(60)
+		Crab.car_move("forward", 75, 10000)
+		time.sleep(6)
+		Crab.car_move("left", 75, 10000)
+		time.sleep(6)
+		Crab.car_move("backward", 75, 10000)
+		time.sleep(6)
+		crab.car_move("right", 75, 10000)
+		time.sleep(6)
+		crab.car_move("clockwise", 75, 10000)
+		time.sleep(6)
+		crab.car_move("counter", 75, 10000)
+		Crab.car_off()
 
 	except KeyboardInterrupt:
 		Crab.car_off()

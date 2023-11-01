d1658c725b52b5c4c6246eb97217a454082b7c38
Aidan-G24 <aidangowe@gmail.com>
2023-11-01 11:30:33 -0400

    Change Config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 pins = {
 	"dir_pins" : [23, 2, 17, 10],
-	"pwm_pins" : [24, 5, 27, 9],
-	"servo_pins" : [25, 4, 22, 11],
+	"pwm_pins" : [9, 5, 27, 24],
+	"servo_pins" : [25, 26, 22, 11],
 	"enc_pins" : [7, 8]
 }

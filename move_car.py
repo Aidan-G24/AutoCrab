5faa71568eef8951c5791305655fd060bd14d736
Aidan-G24 <aidangowe@gmail.com>
2023-11-06 14:44:58 -0500

    first ground test

--- move_car.py
+++ move_car.py
@@ -12,18 +12,18 @@ if __name__ == "__main__":
 
 	try:
 		print("Start Move forward")
-		Crab.car_move("forward", 75, 10000)
+		Crab.car_move("forward", 25, 500)
 		time.sleep(6)
-		Crab.car_move("left", 75, 10000)
+		Crab.car_move("left", 25, 500)
 		time.sleep(6)
-		Crab.car_move("backward", 75, 10000)
+		Crab.car_move("backward", 25, 500)
 		time.sleep(6)
-		Crab.car_move("right", 75, 10000)
+		Crab.car_move("right", 25, 500)
 		time.sleep(6)
-		Crab.car_move("clockwise", 75, 10000)
-		time.sleep(6)
-		Crab.car_move("counter", 75, 10000)
-		Crab.car_off()
+		# Crab.car_move("clockwise", 75, 10000)
+		# time.sleep(6)
+		# Crab.car_move("counter", 75, 10000)
+		# Crab.car_off()
 
 	except KeyboardInterrupt:
 		Crab.car_off()

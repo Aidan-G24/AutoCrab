2a8bab9433216b02d2017bb442bed2f06754e3f9
Aidan-G24 <aidangowe@gmail.com>
2023-11-29 13:19:02 -0500

    test

--- lidarControl.py
+++ lidarControl.py
@@ -8,35 +8,34 @@ class Lidar:
 	def __init__():
 
 		# Create a context object. This object owns the handles to all connected realsense devices
-	    pipeline = rs.pipeline()
+		pipeline = rs.pipeline()
 
-	    # Configure streams
-	    config = rs.config()
-	    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-	    # Start streaming
-	    pipeline.start(config)
+		# Configure streams
+		config = rs.config()
+		config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+		
+		# Start streaming
+		pipeline.start(config)
 
 
 	def check_distance():
 		# return 0 if all clear return 1 if object detected
 
 		frames = pipeline.wait_for_frames()
-        depth = frames.get_depth_frame()
-        if not depth: return 0
-        count = 0
+		depth = frames.get_depth_frame()
+		if not depth: return 0
+		count = 0
 
-        # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
-        coverage = [0]*64
-        for y in range(480):
-            for x in range(640):
-                dist = depth.get_distance(x, y)
-                if 0 < dist and dist < .8:
-                    count += 1
-        
-        if count > (480 * 640)*.10:
-            return 1
+		# Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
+		coverage = [0]*64
+		for y in range(480):
+			for x in range(640):
+				dist = depth.get_distance(x, y)
+				if 0 < dist and dist < .8:
+					count += 1
 
-        return 0
+		if count > (480 * 640)*.10:
+			return 1
 
-    
+		return 0
+
